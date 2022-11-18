2c0094d1f5ed16912cf80b4173e7c00ba7cee687
Yohanna Oliveira <yohannaoliveira50@gmail.com>
2022-11-17 21:49:55 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/coursespring/entities/Order.java
+++ src/main/java/com/educandoweb/coursespring/entities/Order.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import com.educandoweb.coursespring.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -34,6 +36,8 @@ public class Order implements Serializable {
 	private User client;	
 	@OneToMany(mappedBy = "id_order")
 	private Set<OrderItem> items = new HashSet<>();
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)	
+	private Payment payment;
 
 	public Order() {
 	}
@@ -84,6 +88,14 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
